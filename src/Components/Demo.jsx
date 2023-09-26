35b90c427cdc9a174cf3ce238c516ca08c18ea20
Tamal Kundu <tamalkundu2002@gmail.com>
2023-09-27 01:16:30 +0530

    Update Demo.jsx

--- src/Components/Demo.jsx
+++ src/Components/Demo.jsx
@@ -86,7 +86,7 @@ const Demo = () => {
 
         {/* Browse History */}
         <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
-          {allArticles.reverse().map((item, index) => (
+          {allArticles.map((item, index) => (
             <div
               key={`link-${index}`}
               onClick={() => setArticle(item)}
