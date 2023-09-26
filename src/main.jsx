58f04ea31592a1c5af8f14e67bc09e18766bcca2
Tamal Kundu <tamalkundu2002@gmail.com>
2023-09-27 00:32:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import { store } from './services/Store.js';
+import { store } from './services/store.js';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
